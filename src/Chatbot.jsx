8e1c20b57d9229d4deef17254ba1da7a823dd3f0
basant-goswami <basantbharati1999@gmail.com>
2024-04-15 01:00:59 +0530

    third

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -5,7 +5,7 @@ export default function Chatbot() {
     <>
       <div className=" chatbot relative top-[20%] text-center ">
         <h1 className=" text-3xl px-1 font-bold font-serif md:text-5xl">
-          Commucate with our AI Chat bot.
+          Communicate with AI Chat bot.
         </h1>
         <p className=" font-mono md:text-xl mt-4 px-1">
           Fell free to ask anythink or chat with our ai chatbot
